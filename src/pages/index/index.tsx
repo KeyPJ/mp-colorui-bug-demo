f9e6d9ed1c1534daab5594dbe8491f656b78dd66
KeyPJ <pengjian1994@gmail.com>
2021-03-01 21:05:38 +0800

    add mp-colorui

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,6 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
+import { ClFloatButton } from "mp-colorui"
 import './index.scss'
 
 export default class Index extends Component {
@@ -18,6 +19,21 @@ export default class Index extends Component {
     return (
       <View className='index'>
         <Text>Hello world!</Text>
+        <ClFloatButton
+          size='large'
+          bgColor='red'
+          closeWithShadow
+          direction='vertical'
+          move
+          actionList={[
+            {
+              icon: 'lightforbid'
+            },
+            {
+              icon: 'friendfamous'
+            }
+          ]}
+        />
       </View>
     )
   }
